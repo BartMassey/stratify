b233eae532da484bb6aecb7938b4fad15c8c30a9
Bart Massey <bart@cs.pdx.edu>
2008-09-28 02:01:09 -0700

    finished the Haddock

--- Stratify.hs
+++ Stratify.hs
@@ -9,30 +9,38 @@ where
 
 import Data.List (isPrefixOf)
 
--- |Given a list representing a logical "separator", and a
--- sequences of elements that may contain this separator,
+-- |Given a list representing a logical \"separator\", and a
+-- list that may contain occurrences of this separator,
 -- 'stratify' returns a list of the (possibly empty)
--- subsequences "between" instances of the separator.
+-- subsequences \"between\" instances of the separator.
 -- 
 -- 'stratify' is the inverse of 'intercalate'.
--- 	'intercalate' s ('stratify' s l) == l
+-- 
+-- >  intercalate s (stratify s l) == l
+-- 
 -- Also
--- 	'stratify' s ('intercalate' s l) == l
+-- 
+-- >  stratify s (intercalate s l) == l
+-- 
 -- except in the cases where 'intercalate' loses
--- information.  These are: when l == [[]]; when s == [];
--- when s overlaps the elements of l in such a way as to
--- add extra occurrences of s.
+-- information.  These are:
 -- 
--- 	"Intercalated: A body of material interbedded or
--- 	interlaminated with another."
+--    (1) When @l == [[]]@
 -- 
--- 	"Stratified: A term applied to rocks deposited in
--- 	nearly horizontal layers or strata on the earth's
--- 	surface."
+--    (2) When @s == []@
 -- 
--- 	US Geological Survey Bulletin 587
--- 	http://www.nps.gov/history/history/online_books/
--- 	geology/publications/pp/587/glossary.htm	
+--    (3) When @s@ overlaps the elements of @l@ in such a way as to
+--        add extra occurrences of @s@.
+-- 
+-- \"Intercalated\: A body of material interbedded or
+-- interlaminated with another.\"
+-- 
+-- \"Stratified\: A term applied to rocks deposited in
+-- nearly horizontal layers or strata on the earth's
+-- surface.\"
+-- 
+-- \--US Geological Survey Bulletin 587
+-- <http://www.nps.gov/history/history/online_books/geology/publications/pp/587/glossary.htm>
 stratify :: (Eq a) => [a] -> [a] -> [[a]]
 stratify _ [] = []
 stratify [] l = map (:[]) l
@@ -43,15 +51,3 @@ stratify sep l = go l where
     go (c : cs) = (c : l0) : l1
         where (l0 : l1) = go cs
     go [] = [[]]
-
-
--- intercalate "" [] -> ""
--- intercalate "" [""] -> ""
--- intercalate "" l -> concat l
--- intercalate "x" [] -> ""
--- intercalate "x" [""] -> ""
--- intercalate "x" ["",""] -> "x"
--- intercalate "x" ["a"] -> "a"
--- intercalate "x" ["a", "b"] -> "axb"
--- intercalate "x" ["a", ""] -> "ax"
--- intercalate "x" ["", "b"] -> "xb"
